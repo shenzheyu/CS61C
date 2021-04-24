6ee16b14fbcf9bfa6e3c8e1e6afde4fa283eb427
shenzheyu <shenzheyu96@gmail.com>
2021-04-24 18:24:31 +0800

    Accomplish lab 1

--- su20-lab-starter/lab01/ll_cycle.c
+++ su20-lab-starter/lab01/ll_cycle.c
@@ -3,5 +3,17 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *tortoise = head;
+    node *hare = head;
+    while (tortoise != NULL && hare != NULL) {
+        tortoise = tortoise->next;
+        if (tortoise != NULL) {
+            tortoise = tortoise->next;
+        }
+        hare = hare->next;
+        if (tortoise == hare) {
+            return 1;
+        }
+    }
     return 0;
 }
